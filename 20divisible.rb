f5371050757275041e7482263bf27fc74d0c0d50
FURams09 <padin.greg@gmail.com>
2014-10-02 11:05:33 -0400

    Refactored 20 divisible to take any number in a range. Still a brute force attack.

--- 20divisible.rb
+++ 20divisible.rb
@@ -1,29 +1,36 @@
-def findNum
-	currNum = 20
-	foundAnswer = false
+class FindDivisible
+	@@thruNum = 15
+	@@primeDivisors = (1..@@thruNum).to_a
+	def self.findNum
+		currNum = @@thruNum
+		foundAnswer = false
 
-	while foundAnswer == false do
-		if divCheck currNum, 20 #recursively check currNum modulo every number under 20
-			puts 'the smallest number divisible by all numbers 1-20 is ' + currNum.to_s
-			return
-			#don't really need to set foundAnswer to true since return will just end the code but this will be
-			#a good reminder of  why it was there. 
-			#foundAnswer == true
-		else
-			currNum += 1
-		end
+		while foundAnswer == false do
+
+			if divCheck currNum, (@@primeDivisors.length) -1  #recursively check currNum modulo every number under 20
+				puts 'the smallest number divisible by all numbers 1-'+ @@thruNum' is ' + currNum.to_s
+				return
+				#don't really need to set foundAnswer to true since return will just end the code but this will be
+				#a good reminder of  why it was there. 
+				#foundAnswer == true
+			else
+				currNum += 1
+			end
 
 
-	end 
-end
-
-def divCheck (topNumber, bottomNumber)
-	puts topNumber
-	if bottomNumber == 1
-		true
-	elsif (topNumber % bottomNumber == 0) 
-	 	divCheck(topNumber, (bottomNumber -1))
-	 else
-	 	false
+		end 
 	end
+
+	def self.divCheck (topNumber, bottomNumber)
+		puts topNumber.to_s + ' ' + bottomNumber.to_s + ' ' + @@primeDivisors[bottomNumber].to_s
+		if bottomNumber == 0
+			true
+		elsif (topNumber % @@primeDivisors[bottomNumber] == 0) 
+		 	divCheck(topNumber, (bottomNumber -1))
+		 else
+		 	false
+		end
+	end
+
+	
 end
